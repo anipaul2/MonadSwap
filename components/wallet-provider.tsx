6ea9933ab3aebd91673112438ca29a19576c1b20
Aniketh Paul <aniketh131@gmail.com>
2025-11-24 17:17:37 +0530

    Update wallet-provider.tsx

--- components/wallet-provider.tsx
+++ components/wallet-provider.tsx
@@ -1,9 +1,33 @@
 import { farcasterMiniApp as miniAppConnector } from '@farcaster/miniapp-wagmi-connector'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { http, WagmiProvider, createConfig } from 'wagmi'
-import { monadTestnet } from 'wagmi/chains'
+import { defineChain } from 'viem'
 import { injected } from 'wagmi/connectors'
 import { sdk } from '@farcaster/miniapp-sdk'
+
+const monadMainnet = defineChain({
+  id: 143, // Monad mainnet chain ID
+  name: 'Monad',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Monad',
+    symbol: 'MON',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc-mainnet.monadinfra.com'],
+    },
+    public: {
+      http: ['https://rpc3.monad.xyz'],
+    },
+  },
+  blockExplorers: {
+    default: { 
+      name: 'MonadVision', 
+      url: 'https://monadvision.com' 
+    },
+  },
+})
 
 // Create connectors based on environment
 const createConnectors = () => {
